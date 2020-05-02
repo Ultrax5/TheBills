f90224e2918bcf5c69deeff685b503f3d0c57a7f
kulgawcz <stefan.ultrax5@gmail.com>
2020-05-02 14:01:13 +0200

    szeremere login setup

--- src/main/java/TheBills/RedditConnect.java
+++ src/main/java/TheBills/RedditConnect.java
@@ -11,15 +11,26 @@ import net.dean.jraw.oauth.OAuthHelper;
 public class RedditConnect {
     public static UserAgent userAgent = new UserAgent("bot", "com.example.usefulbot", "v6.9", "TheBills");
     public static RedditClient reddit;
-    public static void setUp(String username, String password){
+    public static boolean setUp(String username, String password){
         //"TheBillsV69", "$6qn2vzBFprWV8s"
-        Credentials credentials = Credentials.script(username, password, "18zdrx3lzdrbZw", "vEd1p_OuAF03vHKZIxXuutaUQus");
+        Credentials credentials = null;
+        try {
+            credentials = Credentials.script(username, password, "18zdrx3lzdrbZw", "vEd1p_OuAF03vHKZIxXuutaUQus");
+        } catch (Exception e) {
+        }
+        if(credentials!=null){
+            NetworkAdapter adapter = new OkHttpNetworkAdapter(userAgent);
+            // Authenticate and get a RedditClient instance
+            reddit = OAuthHelper.automatic(adapter, credentials);
+            Subreddit sr = reddit.subreddit("RocketLeague").about();
+            System.out.println(sr.toString());
+            return true;
+        } else {
+            System.out.println("ZLE HASLO XD");
+            return false;
+        }
         // This is what really sends HTTP requests
-        NetworkAdapter adapter = new OkHttpNetworkAdapter(userAgent);
-        // Authenticate and get a RedditClient instance
-        reddit = OAuthHelper.automatic(adapter, credentials);
-        Subreddit sr = reddit.subreddit("RocketLeague").about();
-        System.out.println(sr.toString());
+        
     }
 
 
