dcbcc58e3a5f3b035602c38854d6d079db4da147
d4nnysh <pitras9910@gmail.com>
2020-05-02 14:53:25 +0200

    zmianka posta

--- src/main/java/TheBills/ShowPost.java
+++ src/main/java/TheBills/ShowPost.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class Post extends JPanel implements ActionListener{
+public class ShowPost extends JPanel implements ActionListener{
     private JButton user= new JButton();
     private JLabel like= new JLabel();
     private JButton comments= new JButton();
@@ -13,7 +13,7 @@ public class Post extends JPanel implements ActionListener{
     private JButton read= new JButton("Read it for me!");
     private String text= "";
 
-    public Post(){
+    public ShowPost(){
         user.setText("Posted by: ");//+username, +godzina
         subreddit.setText("r/");//+sub
         like.setText(420+ " likes");//liczba lajkow
--- src/main/java/TheBills/Window.java
+++ src/main/java/TheBills/Window.java
@@ -6,22 +6,42 @@ import java.awt.event.*;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
+import TheBills.LoginUI;
+
 public class Window implements ActionListener{
     private JFrame frame= new JFrame("The Bills Reddit");
     private JPanel panel= new JPanel();
+    private BoxLayout layout = new BoxLayout(panel, 1);   
     private String name= "";
     private JLabel greeting= new JLabel();
+    private ShowPost curPost;
+    private JButton next= new JButton("Next post");
+    private JButton logout= new JButton("Log out");
+
+    private ShowPost[] posts;
     public Window(){
+        //dodaj wszystkie posty do arraya posts
+        panel.setLayout(layout);
+        //dodaj post do panelu
         greeting.setText("Hello, "+name);
         panel.add(greeting);
+        panel.add(next);
         frame.setContentPane(panel);
         frame.setSize(1000,800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
         
     }
     public void actionPerformed(ActionEvent u){
+        if(u.getSource()==next){
+            //wyswietl nowego posta
+        }
+        else if(u.getSource()==logout){
+            frame.dispose();
+            LoginUI log= new LoginUI();
+        }
         
     }
 }
