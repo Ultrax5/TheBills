6f3e02114fe0c04c931f0531b961fa755b35398e
kulgawcz <stefan.ultrax5@gmail.com>
2020-05-02 13:48:54 +0200

    podloczenie do api wariaty

--- src/main/java/TheBills/App.java
+++ src/main/java/TheBills/App.java
@@ -14,6 +14,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        RedditConnect.setUp(null, null);
         LoginUI loginUI = new LoginUI();
     }
 }
---
+++ src/main/java/TheBills/RedditConnect.java
@@ -0,0 +1,25 @@
+package TheBills;
+
+import net.dean.jraw.RedditClient;
+import net.dean.jraw.http.NetworkAdapter;
+import net.dean.jraw.http.OkHttpNetworkAdapter;
+import net.dean.jraw.http.UserAgent;
+import net.dean.jraw.models.Subreddit;
+import net.dean.jraw.oauth.Credentials;
+import net.dean.jraw.oauth.OAuthHelper;
+
+public class RedditConnect {
+    public static UserAgent userAgent = new UserAgent("bot", "com.example.usefulbot", "v6.9", "TheBills");
+    public static void setUp(String username, String password){
+        Credentials credentials = Credentials.script("TheBillsV69", "$6qn2vzBFprWV8s",
+    "18zdrx3lzdrbZw", "vEd1p_OuAF03vHKZIxXuutaUQus");
+
+        // This is what really sends HTTP requests
+        NetworkAdapter adapter = new OkHttpNetworkAdapter(userAgent);
+        // Authenticate and get a RedditClient instance
+        RedditClient reddit = OAuthHelper.automatic(adapter, credentials);
+        Subreddit sr = reddit.subreddit("RocketLeague").about();
+        System.out.println(sr.toString());
+    }
+
+}
