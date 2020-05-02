70798fc2d17d5fe22227bbab42354722d90297bb
kulgawcz <stefan.ultrax5@gmail.com>
2020-05-02 13:52:26 +0200

    ADD LOGIN

--- src/main/java/TheBills/RedditConnect.java
+++ src/main/java/TheBills/RedditConnect.java
@@ -10,16 +10,18 @@ import net.dean.jraw.oauth.OAuthHelper;
 
 public class RedditConnect {
     public static UserAgent userAgent = new UserAgent("bot", "com.example.usefulbot", "v6.9", "TheBills");
+    public static RedditClient reddit;
     public static void setUp(String username, String password){
-        Credentials credentials = Credentials.script("TheBillsV69", "$6qn2vzBFprWV8s",
-    "18zdrx3lzdrbZw", "vEd1p_OuAF03vHKZIxXuutaUQus");
-
+        //"TheBillsV69", "$6qn2vzBFprWV8s"
+        Credentials credentials = Credentials.script(username, password, "18zdrx3lzdrbZw", "vEd1p_OuAF03vHKZIxXuutaUQus");
         // This is what really sends HTTP requests
         NetworkAdapter adapter = new OkHttpNetworkAdapter(userAgent);
         // Authenticate and get a RedditClient instance
-        RedditClient reddit = OAuthHelper.automatic(adapter, credentials);
+        reddit = OAuthHelper.automatic(adapter, credentials);
         Subreddit sr = reddit.subreddit("RocketLeague").about();
         System.out.println(sr.toString());
     }
+
+
 
 }
